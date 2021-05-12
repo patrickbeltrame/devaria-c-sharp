7e92bc023f82ac9c9110c319130df922c7f4a3e3
Patrick Rodrigues <p.rodrigues0507@gmail.com>
2021-05-12 10:09:07 -0300

    meu primeiro commit C#

---
+++ TesteVariavelConstante/TesteVariavelConstante/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TesteVariavelConstante
+{
+    class Program
+    {
+        static void Main(string[] argumentos)
+        {
+            const string constante = "Uma constante que não muda";
+
+            if(argumentos.Length == 0)
+            {
+                Console.WriteLine("Favor informar um numero inteiro");
+                return;
+            }
+
+            int inteiro;
+            bool test = int.TryParse(argumentos[0], out inteiro);
+
+            if(test == false)
+            {
+                Console.WriteLine("O argumento informado ná é um número inteiro");
+                return;
+            }
+
+            Console.WriteLine(constante);
+            Console.WriteLine("O número informado foi: " + inteiro);
+        }
+    }
+}
