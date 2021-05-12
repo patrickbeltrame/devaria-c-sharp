b87eea31e71c9761b6269752eafdb5dfc1ae4993
Patrick Rodrigues <p.rodrigues0507@gmail.com>
2021-05-12 00:38:03 -0300

    Programa Olá mundo

---
+++ OlaMundo/OlaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo, meu primeiro código C# Devaria 2021");
+        }
+    }
+}
